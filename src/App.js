e2fcf1937efd6aa466a2ed5f30420d0fa7789585
naveen krishna <ujwalar6@gmail.com>
2020-06-19 15:25:56 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+import Sidebar from './components/sidebar'
+import Sidebar1 from './components/sidebar1'
+import Introduction from './components/introduction'
+import About from './components/about'
+import Timeline from './components/timeline'
+
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        	<Sidebar></Sidebar>
+          <Sidebar/> 
+          <div id="colorlib-main">
+					<Introduction></Introduction>
+					<About></About>
+					<Timeline></Timeline>
+          	</div>
+       </div>
+    );
+  }
+}
+
+export default App;
