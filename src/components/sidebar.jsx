a1c541cc565db3865ab355133084223dbdd933be
naveen krishna <ujwalar6@gmail.com>
2020-06-21 03:44:57 -0400

    square stylings

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,10 +4,11 @@ export default class Sidebar extends Component {
   render() {
     return (
       <div>
-        <div>
+        <div className='container'>
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
+              <button onClick=''></button>
               <div className="author-img" />
               <h1 id="colorlib-logo"><a href="index.html">Dhruv Barochiya</a></h1>
               <span className="email"><i className="icon-mail"></i> dhruv.barochia34788@gmail.com</span>
@@ -23,16 +24,13 @@ export default class Sidebar extends Component {
                 </ul>
               </div>
             </nav>
-            <nav id="colorlib-main-menu">
-              <ul>
-                <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-                <li><a href="https://twitter.com/ddbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
-                <li><a href="https://www.instagram.com/ddbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
-                <li><a href="https://www.linkedin.com/in/dbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
-                <li><a href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
-              </ul>
-            </nav>
+            <div className="technology-wrapper">
+              <div className="technology"><div className="square" ></div><div>1270</div></div>
+              <div className="technology"><div className="square" ></div><div>1272</div></div>
+              <div className="technology"><div className="square" ></div><div>1274</div></div>
+              <div className="technology"><div className="square" ></div><div>1276</div></div>
+
+            </div>
             <div className="colorlib-footer">
               <p><small>
                   Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
