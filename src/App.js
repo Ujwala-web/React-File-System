cc63ae2cec4e2c2e9420e293b278fcf316f870b8
naveen krishna <ujwalar6@gmail.com>
2020-06-22 13:07:28 -0400

    added few things to be reverted

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ class App extends Component {
       <div className='header'>Intel Header</div>
       <div id="colorlib-page">
         	<Sidebar toggleShow={this.openSideBar}/>
+          <Sidebar toggleShow={this.openSideBar}/>
+          <Sidebar toggleShow={this.openSideBar}/>
           {
             this.state.show &&  <TechSidebar toggleShow={this.closeSideBar}/>
           }
